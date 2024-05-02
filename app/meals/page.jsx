f33aecf4e382944aa5b4a6a80f02fae43308801a
ndikrp <andhikarizky36@gmail.com>
2024-05-02 20:46:47 +0700

    add custom loading

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,8 +1,15 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid.module";
+import { getMeals } from "../lib/meals";
+import { Suspense } from "react";
 
-export default function MealsPage() {
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -18,7 +25,11 @@ export default function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Getting Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
